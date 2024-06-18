0d7ced245fc9df3914b9a51ec4b7f1b8bcac5a13
wasoract <wasoract@skiff.com>
2024-06-18 22:15:07 +0400

    FarmButton

--- src/components/FarmButton.tsx
+++ src/components/FarmButton.tsx
@@ -1,9 +1,24 @@
 import { Component } from 'solid-js'
 
+import farmIcon from "../assets/farm_48x46.76_3x.png"
+import { Column } from './Column'
 import css from './FarmButton.module.scss'
+
+const t = {
+	label: "Farm",
+}
 
 export const FarmButton: Component = () => {
 	return (
-		<div class={css.button} />
+		<Column class={css.button}>
+			<img
+				width="48"
+				height="46.76"
+				src={farmIcon}
+			/>
+			<div class={css.label}>
+				{t.label}
+			</div>
+		</Column>
 	)
 }
