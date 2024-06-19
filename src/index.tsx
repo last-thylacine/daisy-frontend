7b1bb2e9a560565104536a84384096b8fe1e660f
wasoract <wasoract@skiff.com>
2024-06-20 02:25:47 +0400

    this fix really fixes it

--- src/index.tsx
+++ src/index.tsx
@@ -16,7 +16,7 @@ WebApp.ready()
 WebApp.expand()
 
 render(() => (
-	<Router root={App}>
+	<Router root={App} base={import.meta.env.BASE_URL}>
 		<Route info={{ tab: '/earn'    }} path={"/earn"}        component={EarnPage} />
 		<Route info={{ tab: '/grow'    }} path={["/", "/grow"]} component={GrowPage} />
 		<Route info={{ tab: '/friends' }} path={"/friends"}     component={FriendsPage} />
