2076fd7f86ec1468bc71727139cfcabb8aabdad6
wasoract <wasoract@skiff.com>
2024-06-20 23:39:01 +0400

    fix

--- src/components/TopUI.tsx
+++ src/components/TopUI.tsx
@@ -2,7 +2,7 @@ import { Component } from 'solid-js'
 
 import { Chip } from './Chip'
 import { Row } from './Row'
-import css from './TopUI.module.scss'
+// import css from './TopUI.module.scss'
 
 export const TopUI: Component = () => {
 	return (
