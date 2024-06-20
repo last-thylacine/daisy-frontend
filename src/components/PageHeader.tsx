948e867509c43ffb1cc025949eed1711167b20db
wasoract <wasoract@skiff.com>
2024-06-20 07:00:38 +0400

    implement PageHeader

---
+++ src/components/PageHeader.tsx
@@ -0,0 +1,35 @@
+import { Component } from 'solid-js'
+
+import css from './PageHeader.module.scss'
+import { Column } from './Column'
+
+type Props = {
+	icon?: string
+	iconWidth?: string
+	iconHeight?: string
+	title: string
+	subtitle?: string
+}
+
+export const PageHeader: Component<Props> = (props) => {
+	return (
+		<Column class={css.container}>
+			{props.icon && (
+				<img
+					class={css.icon}
+					width={props.iconWidth}
+					height={props.iconHeight}
+					src={props.icon}
+				/>
+			)}
+			<div class={css.title}>
+				{props.title}
+			</div>
+			{props.subtitle && (
+				<div class={css.subtitle}>
+					{props.subtitle}
+				</div>
+			)}
+		</Column>
+	)
+}
