e82f6fd5e3f79af03e2b50b78d14f6fe9d510944
wasoract <wasoract@skiff.com>
2024-06-21 10:48:54 +0400

    add images & lint

--- src/components/UpgradeDrawer.tsx
+++ src/components/UpgradeDrawer.tsx
@@ -1,24 +1,24 @@
-import { Component, For } from 'solid-js'
+import { Component } from 'solid-js'
 
-import { app } from '../common/app'
-import farmImage from "../assets/farm_82.13x80_3x.png"
+// import { app } from '../common/app'
+// import farmImage from "../assets/farm_82.13x80_3x.png"
 import { Drawer } from './Drawer'
-import { UpgradeListItem } from './UpgradeListItem'
-import css from './UpgradeDrawer.module.scss'
+// import { UpgradeListItem } from './UpgradeListItem'
+// import css from './UpgradeDrawer.module.scss'
 
-const t = {
-	confirm: "Go ahead",
-}
+// const t = {
+// 	confirm: "Go ahead",
+// }
 
 type Props = {
 	onClose: () => void
 }
 
 export const UpgradeDrawer: Component<Props> = (props) => {
-	const { store } = app
+	// const { store } = app
 	return (
 		<Drawer onClose={props.onClose}>
-			<img
+			{/* <img
 				draggable="false"
 				width="82.13"
 				height="80"
@@ -27,7 +27,7 @@ export const UpgradeDrawer: Component<Props> = (props) => {
 			<div class={css.subtitle}>{t.subtitle}</div>
 			<For each={store.upgrades}>
 				{(upgrade) => <UpgradeListItem {...upgrade} />}
-			</For>
+			</For> */}
 		</Drawer>
 	)
 }
