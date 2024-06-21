b86286a380b1708dfdbd7a106974d3597deaac8f
wasoract <wasoract@skiff.com>
2024-06-21 11:25:19 +0400

    flexible layout

--- src/components/Daisy.tsx
+++ src/components/Daisy.tsx
@@ -2,11 +2,13 @@ import { Component } from 'solid-js'
 
 import { app } from "../common/app"
 import flowerImage from "../assets/flower.svg"
+import css from './Daisy.module.scss'
 
 export const Daisy: Component = () => {
 	const { tap } = app
 	return (
 		<img
+			class={css.daisy}
 			draggable="false"
 			width="280"
 			height="280"
