daf175aee97a5ae87e8fded9521f6d0c111dfd57
wasoract <wasoract@skiff.com>
2024-06-30 11:23:36 +0400

    testing daily bonus

--- src/components/DailyBonusDrawer.tsx
+++ src/components/DailyBonusDrawer.tsx
@@ -1,5 +1,6 @@
 import { Component, For } from 'solid-js'
 
+import { api } from '../common/api'
 import { DAILY_BONUS } from '../common/DAILY_BONUS'
 import giftIcon from '../assets/images/gift.svg'
 import { Button } from './Button'
@@ -10,6 +11,7 @@ import css from './DailyBonusDrawer.module.scss'
 const t = {
 	title: "Daily Bonus",
 	subtitle: "Get coins for logging into the game daily without skipping. Tap “Get Daily Bonus” every day",
+	get: "Get Daily Bonus",
 	tomorrow: "Come back tomorrow",
 }
 
@@ -18,6 +20,9 @@ type Props = {
 }
 
 export const DailyBonusDrawer: Component<Props> = (props) => {
+	const handleButtonClick = () => {
+		api.dailybonus_use()
+	}
 	return (
 		<Drawer onClose={props.onClose}>
 			<img
@@ -39,7 +44,13 @@ export const DailyBonusDrawer: Component<Props> = (props) => {
 					)}
 				</For>
 			</div>
-			<Button disabled>{t.tomorrow}</Button>
+			<Button
+				onClick={handleButtonClick}>
+				{t.get}
+			</Button>
+			{/* <Button disabled>
+				{t.tomorrow}
+			</Button> */}
 		</Drawer>
 	)
 }
