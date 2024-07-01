7a2b627b6674ea268ae155822d024c66f7b87d50
wasoract <wasoract@skiff.com>
2024-07-01 12:02:16 +0400

    testing ws

--- src/common/ws.ts
+++ src/common/ws.ts
@@ -6,9 +6,10 @@ const init = () => {
 	console.log('ws.init')
 	try {
 		socket = new WebSocket(WS_URL)
-	
+		// {"type":"player_update","Energy":998,"MaxEnergy":1000,"Coins":46431,"Coins_per_tap":1,"Timestamp":1719820804748}	1719820804.7784278
 		socket.addEventListener("message", (event) => {
 			console.log("Message from server ", event.data);
+			console.log(typeof event.data)
 		})
 	} catch (e) {
 		console.error('Error initializing WebSocket: ', e)
